4c9df41fc80bc0ab289220b46004744ab954d429
Jashele T <jashele@Jasheles-MacBook-Air.local>
2019-07-30 13:40:00 -0700

    added export

--- src/data.js
+++ src/data.js
@@ -1,15 +1,13 @@
 // STEP 1 - Get the modules here exported before building out your components
-import { numbers } from "./components/ButtonComponents/NumberButtons/Numbers";
-import { operators } from "./components/ButtonComponents/OperatorButtons/Operators";
-import { specials } from "./components/ButtonComponents/SpecialButtons/Specials";
 // Don't worry about bringing all of these into the same file.
 // Export them as necessary and import each array into its appropriate
 // file. No real tricks here just be aware of what is in each array
 // and how you'll access the data.
 
-const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+export const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
 
-const operators = [
+
+export const operators = [
   {
     char: "/",
     value: "/"
@@ -32,4 +30,5 @@ const operators = [
   }
 ];
 
-const specials = ["C", "+/-", "%"];
+export const specials = ["C", "+/-", "%"];
+
